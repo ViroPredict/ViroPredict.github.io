3d65bd6c35ac058a63049a2d48f24389c759a023
qyerer <152987814+qyerer@users.noreply.github.com>
2024-05-09 21:04:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const HLA_G_valueEl = document.getElementById('HLA_G_value')
-const KRT6A_valueEl = document.getElementById('KRT6A_value')
-const LOC105378751_valueEl = document.getElementById('LOC105378751_value')
-const NTRK3_valueEl = document.getElementById('NTRK3_value')
-const RASSF6_valueEl = document.getElementById('RASSF6_value')
-const ROBO4_valueEl = document.getElementById('ROBO4_value')
-const SEZ6L_valueEl = document.getElementById('SEZ6L_value')
+const SEZ6_valueEl = document.getElementById('SEZ6_value')
+const AKR1C1_valueEl = document.getElementById('AKR1C1_value')
+const LILRA5_valueEl = document.getElementById('LILRA5_value')
+const NCKAP5_valueEl = document.getElementById('NCKAP5_value')
+const SETD9_valueEl = document.getElementById('SETD9_value')
 const calculateEl = document.getElementById('calculate')
 const clipboardEl = document.getElementById('clipboard')
 const resultEl = document.getElementById('result')
@@ -20,36 +18,30 @@ clipboardEl.addEventListener('click', () => {
 })
 
 calculateEl.addEventListener('click', () => {
-    const HLA_G_value = +HLA_G_valueEl.value
-    const KRT6A_value = +KRT6A_valueEl.value
-    const LOC105378751_value = +LOC105378751_valueEl.value
-    const NTRK3_value = +NTRK3_valueEl.value
-    const RASSF6_value = +RASSF6_valueEl.value
-	const ROBO4_value = +ROBO4_valueEl.value
-	const SEZ6L_value = +SEZ6L_valueEl.value
+    const SEZ6_value = +SEZ6_valueEl.value
+    const AKR1C1_value = +AKR1C1_valueEl.value
+    const LILRA5_value = +LILRA5_valueEl.value
+    const NCKAP5_value = +NCKAP5_valueEl.value
+    const SETD9_value = +SETD9_valueEl.value
 	
 
-    resultEl.innerText = generateResult(HLA_G_value , KRT6A_value, LOC105378751_value, NTRK3_value, RASSF6_value, ROBO4_value, SEZ6L_value)
+    resultEl.innerText = generateResult(SEZ6_value , AKR1C1_value, LILRA5_value, NCKAP5_value, SETD9_value)
 })
 
-function generateResult(HLA_G_value , KRT6A_value, LOC105378751_value, NTRK3_value, RASSF6_value, ROBO4_value, SEZ6L_value) {
+function generateResult(SEZ6_value , AKR1C1_value, LILRA5_value, NCKAP5_value, SETD9_value) {
 
-    let HLA_G_points = HLA_G_value * 0.8457
-    let KRT6A_points = KRT6A_value * 0.6812 - 1e-10
-    let LOC105378751_points = LOC105378751_value * 1.3234 - 5e-10
-    let NTRK3_points = NTRK3_value * 4.7261 - 1e-09
-    let RASSF6_points = RASSF6_value * (-0.4312) + 12.074
-    let ROBO4_points = ROBO4_value *  2.3901 - 1e-09
-    let SEZ6L_points = SEZ6L_value * 2
-    let sum_points = HLA_G_points + KRT6A_points + LOC105378751_points + NTRK3_points + RASSF6_points + ROBO4_points + SEZ6L_points
+    let SEZ6_points = SEZ6_value * (-2.2222) + 100
+    let AKR1C1_points = AKR1C1_value * 0.0418 + 2e-15
+    let LILRA5_points = LILRA5_value * 2.4875 + 7e-15
+    let NCKAP5_points = NCKAP5_value * 1.543  - 2e-07
+    let SETD9_points = SETD9_value * 0.7337  - 2e-07
+    let sum_points = SEZ6_points + AKR1C1_points + LILRA5_points + NCKAP5_points + SETD9_points 
 	
-    let generatedPassword = 'HLA_G_points:' + HLA_G_points + '\n'
-	+ 'KRT6A_points:' + KRT6A_points + '\n'
-	+ 'LOC105378751_points:' + LOC105378751_points + '\n'
-	+ 'NTRK3_points:' + NTRK3_points + '\n'
-	+ 'RASSF6_points:' + RASSF6_points + '\n'
-	+ 'ROBO4_points:' + ROBO4_points + '\n'
-	+ 'SEZ6L_points:' + SEZ6L_points + '\n'
+    let generatedPassword = 'SEZ6_points:' + SEZ6_points + '\n'
+	+ 'AKR1C1_points:' + AKR1C1_points + '\n'
+	+ 'LILRA5_points:' + LILRA5_points + '\n'
+	+ 'NCKAP5_points:' + NCKAP5_points + '\n'
+	+ 'SETD9_points:' + SETD9_points + '\n'
 	+ 'sum_points:' + sum_points + '\n'
 
     const finalResult = generatedPassword
